a1adc68a9f9b97077bcbb8ce6c6edbf46ec16901
darshilll <darsgajul2003@gmail.com>
2024-08-04 02:06:34 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
+     <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
