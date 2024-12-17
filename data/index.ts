a3d8f46c1e6a5eef2cb968700449319ebdab2653
darshilgajul <darshig@raacom.in>
2024-12-17 23:04:43 +0530

    update

--- data/index.ts
+++ data/index.ts
@@ -43,6 +43,20 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
+    title: "Identable",
+    des: "Identable, a SaaS platform that leverages AI to revolutionize social media content creation and scheduling.",
+    img: "/image.png",
+    iconLists: [
+      "/ang.svg",
+      "/nod.svg",
+      "/expr.png",
+      "/mo.svg",
+      "/tail.svg",
+    ],
+    link: "https://www.identable.io/",
+  },
+  {
+    id: 2,
     title: "Taska - Your digital task assistant",
     des: "This platform provides a user-friendly interface to optimize task assignment, monitor progress, and facilitate collaboration.",
     img: "/taska.png",
@@ -57,7 +71,7 @@ export const projects = [
     link: "https://taska-fwm0.onrender.com",
   },
   {
-    id: 2,
+    id: 3,
     title: "Prismify: Connect, Share, Discover.",
     des: "A social media platform which include user authentication, dynamic feed, profile customization, post creation and management.",
     img: "/prismify.png",
@@ -65,7 +79,7 @@ export const projects = [
     link: "https://prismifycommunity.netlify.app",
   },
   {
-    id: 3,
+    id: 4,
     title: "Craavee: Explore, Cook, Share ",
     des: "A feature-rich recipe website that empowers users to search for recipes, view ingredients,follow step-by-step cooking instructions.",
     img: "/crave.png",
@@ -77,16 +91,26 @@ export const projects = [
 export const workExperience = [
   {
     id: 1,
+    title: "RAACOM Infosystems Private Limited",
+    desc: "Backend Developer",
+    desc1:
+      "Designed and developed RESTful APIs using Node.js and Express, enhancing API reliability and performance while maintaining clean, well-documented code, ensuring seamless data exchange and integration with front-end applications.",
+
+    date: "September 2024 - December 2024",
+    tech: ["Node.js","Express.js", "Tailwind CSS", "TypeScript", "MongoDB"],
+  },
+  {
+    id: 2,
     title: "GirlScript Summer Of Code",
     desc: "Open Source Contributor",
     desc1:
       "Led contributions to various open-source projects using MERN stack, TypeScript, Next.js, Tailwind CSS, and Firebase. Innovated solutions and improved project functionality under the guidance of mentorship of the Girlscript community.",
 
-    date: "May 2024 - Present",
+    date: "May 2024 - August 2024",
     tech: ["MERN", "Next.js", "Tailwind CSS", "TypeScript", "Firebase"],
   },
   {
-    id: 2,
+    id: 3,
     title: "CognoRise Infotech",
     desc: "Web Development Intern",
     desc1:
