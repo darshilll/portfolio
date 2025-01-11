6ee422e78a3da5da6717f95167c34e702da2db07
darshilll <darsgajul2003@gmail.com>
2025-01-11 23:31:55 +0530

    footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <p className="border-t px-6 border-gray-500 mx-4"></p>
       <div className="flex justify-between items-center flex-col-reverse md:flex-row">
         <p className="text-xs mt-2 mb-8 lg:mb-10 text-gray-400 px-4">
-          &copy; 2024 Darshil Gajul.
+          &copy; 2025 Darshil Gajul.
         </p>
         <p className="flex gap-4 px-4 pb-2 mt-3 lg:-mt-3">
           {socialMedia.map(({ img, id, link }) => (
